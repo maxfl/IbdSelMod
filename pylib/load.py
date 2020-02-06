c597eec511d45de063dde8bcb07bb0fbc45029b1
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2020-02-06 15:08:52 +0300

    minor

--- pylib/load.py
+++ pylib/load.py
@@ -1,12 +1,13 @@
 import ROOT
-ROOT.gSystem.AddIncludePath("-Iselector/stage1")
-ROOT.gSystem.AddIncludePath("-Iselector/stage2")
+ROOT.gSystem.AddIncludePath("-IIbdSel")
+ROOT.gSystem.AddIncludePath("-IIbdSel/selector/stage1")
+ROOT.gSystem.AddIncludePath("-IIbdSel/selector/stage2")
 # ROOT.gSystem.AddIncludePath("-Iselector/SelectorFramework/core")
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 import numpy as np
 error = np.zeros(1, dtype='int32')
-for line in (".L selector/stage1/stage1_main.cc+", ".L selector/stage2/stage2_main.cc+"):
+for line in (".L IbdSel/selector/stage1/stage1_main.cc+", ".L IbdSel/selector/stage2/stage2_main.cc+"):
     ROOT.gROOT.ProcessLine(line, error)
     if error[0]:
         raise Exception('Error processing: '+line)
